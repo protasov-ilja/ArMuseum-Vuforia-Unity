f0ab0e2dab8be96c89bbcea11d932b90008b780c
ilya-protasov <pfltriot@gmail.com>
2020-05-30 20:46:51 +0300

    added mapManger to collect all map data in it

--- Assets/AppAssets/Scripts/ARNavigation/NavSystem/NavigationDestination.cs
+++ Assets/AppAssets/Scripts/ARNavigation/NavSystem/NavigationDestination.cs
@@ -1,14 +1,22 @@
+using System;
 using ARMuseum.Scriptables;
+using TMPro;
 using UnityEngine;
 
 namespace ARMuseum
 {
     public class NavigationDestination : MonoBehaviour
     {
+        [SerializeField] private TMP_Text _hallName;
         [SerializeField] private GameObject _destinationPointer;
         [SerializeField] private HallDataSO _hallData;
 
         public HallDataSO HallData => _hallData;
+
+        private void Start()
+        {
+            _hallName.text = _hallData.HallName;
+        }
 
         public void ActivateDestinationPointer(bool isActive)
         {
